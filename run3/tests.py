fdda0280825112d2336abeb39354a38737e2c3b6
Lawrence Zhang <lawrencezhang@college.harvard.edu>
2023-04-10 12:20:14 -0400

    Testing framework

---
+++ run3/tests.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import mock
+import socket
+import selectors
+
+class TestDatabaseMethods(unittest.TestCase):
+    pass
+
+class TestServerMethods(unittest.TestCase):
+    pass
+
+class TestUserInputMethods(unittest.TestCase):
+    pass
+
+class TestClientMethods(unittest.TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
